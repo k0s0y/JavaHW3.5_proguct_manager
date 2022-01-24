f6066c148184fb511332a69aa6b72f5141ba6c29
Timofei <t.a.maksimenko@ya.ru>
2022-01-24 15:05:31 +0300

    Исправлены ошибки, добавлен @BeforeEach, добавлены тесты

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -1,5 +1,6 @@
 package ru.netology.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
@@ -13,61 +14,82 @@ class ProductManagerTest {
     Product second = new Book(2, "1984", 550, "George Orwell");
     Product third = new Smartphone(3, "3310", 3000, "Nokia");
     Product fourth = new Smartphone(4, "Galaxy S22", 80000, "Samsung");
+    Product fifth = new Smartphone(5, "Galaxy S21", 65000, "Samsung");
+    Product sixth = new Product(1,"Uncorrect product", 800); //добавлен продукт, без указания типа, для отработки менеджера
     private ProductRepository repository = new ProductRepository();
     private ProductManager manager = new ProductManager(repository);
 
+    @BeforeEach
     public void setUp() {
         manager.add(first);
         manager.add(second);
         manager.add(third);
         manager.add(fourth);
-    }
-
-    @Test
-    public void shouldAddNewItem() {
-        manager.add(first);
-        Product[] expected = new Product[]{first};
-        Product[] actual = manager.getAll();
-        assertArrayEquals(expected, actual);
+        manager.add(fifth);
+        manager.add(sixth);
     }
 
     @Test
     public void shouldSearchIfIncludeNameInBook() {
-        setUp();
         Product[] expected = new Product[]{first};
         Product[] actual = manager.searchBy("Fight Club");
         assertArrayEquals(expected, actual);
     }
 
     @Test
+    public void shouldSearchIfNameNotIncludeInBook() {
+        Product[] expected = new Product[]{};
+        Product[] actual = manager.searchBy("NewText");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void shouldSearchIfIncludeNameInSmartphone() {
-        setUp();
         Product[] expected = new Product[]{third};
         Product[] actual = manager.searchBy("3310");
         assertArrayEquals(expected, actual);
     }
 
     @Test
+    public void shouldSearchIfNameNotIncludeInSmartphone() {
+        Product[] expected = new Product[]{};
+        Product[] actual = manager.searchBy("NewModel");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void shouldSearchIfIncludeAuthorInBook() {
-        setUp();
         Product[] expected = new Product[]{second};
         Product[] actual = manager.searchBy("George Orwell");
         assertArrayEquals(expected, actual);
     }
 
     @Test
+    public void shouldSearchIfManufactureIncludeInProduct() {
+        Product[] expected = new Product[]{third};
+        Product[] actual = manager.searchBy("3310");
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
     public void shouldSearchIfIncludeNotFullAuthorNameInBook() {
-        setUp();
         Product[] expected = new Product[]{second};
         Product[] actual = manager.searchBy("Orwell");
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldSearchIfIncludeManufactureInSmartphone() {
-        setUp();
-        Product[] expected = new Product[]{fourth};
+    public void shouldGiveTwoProductsWithSameParams() {
+        Product[] expected = new Product[]{fifth, fourth};
         Product[] actual = manager.searchBy("Samsung");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGiveAllProductsInAdded() {
+        Product[] expected = new Product[]{sixth, fifth, fourth, third, second, first};
+        Product[] actual = manager.getAll();
         assertArrayEquals(expected, actual);
     }
 
